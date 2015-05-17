ce731950867ff539318b85962bd8f66f9726f356
Souleymane Couliably <bina.coulibaly@gmail.com>
2015-05-16 23:35:50 -0400

    Started to create the html for article creation, using the html form helper.

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -2,4 +2,11 @@
 
 @section('content')
 	<h1>Add New Article</h1>
+	
+	{!! Form::open() !!}
+		<div class="form-group">
+			{!! Form::label('title', 'Title') !!}	
+			{!! Form::text('title', null, ['class' => 'form-control']) !!} 
+		</div>
+	{!! Form::close() !!}
 @stop
