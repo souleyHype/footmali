4d6ea90eb676ab40a5897aa4557298d3797cbefc
Souleymane Couliably <bina.coulibaly@gmail.com>
2015-05-16 22:11:19 -0400

    Added columns to articles migragtion and ran migration command.

--- database/migrations/2015_05_17_012449_create_articles_table.php
+++ database/migrations/2015_05_17_012449_create_articles_table.php
@@ -12,7 +12,15 @@ class CreateArticlesTable extends Migration {
 	 */
 	public function up()
 	{
-		//
+		Schema::create('articles', function($table)
+		{
+			$table->increments('id');
+			$table->text('title');
+			$table->text('lead');
+			$table->text('body');
+			$table->text('summary');
+			$table->timestamps();
+		});
 	}
 
 	/**
@@ -22,7 +30,7 @@ class CreateArticlesTable extends Migration {
 	 */
 	public function down()
 	{
-		//
+		Schema::drop('articles');
 	}
 
 }
